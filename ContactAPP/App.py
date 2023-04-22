5ac70085e8b1e5a1a9fb002c95fce95e4cb91281
ElGirasolMaestro <grisol225@gmail.com>
2023-04-22 08:21:38 -0500

    error password

--- ContactAPP/App.py
+++ ContactAPP/App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request;
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
@@ -8,13 +8,20 @@ app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'flaskcontacts'
 mysql = MySQL(app)
 
-@app.route('/')
+@app.route('/') 
 def Index():
     return render_template('index.html')
 
-@app.route('/add_contact')
+@app.route('/add_contact', methods=['POST'])
 def add_contact():
-    return 'add contact'
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute('INSTER INTO contacts (fullname, phone, email) VALUES (%s, %s, %s)',(fullname, phone, email))
+        mysql.connection.commit()
+        return 'received'
 
 @app.route('/edit')
 def edit_contact():
