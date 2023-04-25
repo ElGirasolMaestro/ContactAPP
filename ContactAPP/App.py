d1449004a771e12f924473725f236d005d2ba0b3
ElGirasolMaestro <grisol225@gmail.com>
2023-04-25 01:17:33 -0500

    edit and delete routes working/ issues with id refreshing

--- ContactAPP/App.py
+++ ContactAPP/App.py
@@ -23,16 +23,42 @@ def index():
         mysql.connection.commit()
         cur.close()
         return redirect('/users')
-    return render_template('index.html')
+    return render_template('index.html') 
+
 
 @app.route('/users')
 def users():
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM users")
-    if resultValue > 0:
+    if resultValue >= 0:
         userDetails = cur.fetchall()
         return render_template('users.html',userDetails=userDetails)
+  
+@app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT name, email FROM users WHERE id=%s", (id,))
+    userDetails = cur.fetchone()
+    cur.close()
     
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email'] 
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE users SET name=%s, email=%s WHERE id=%s", (name, email, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/users')
     
+    return render_template('edit_user.html', name=userDetails[0], email=userDetails[1])
+
+@app.route('/delete_user/<int:id>')
+def delete_user(id):
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM users WHERE id=%s", (id,))
+    mysql.connection.commit()
+    cur.close()
+    return redirect('/users')
+
 if __name__ == '__main__':
     app.run(debug=True)
